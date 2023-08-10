9b9058b6f3a7361d98c3d1a649f69a7b66f11ab2
jessicalyruiz <jessicalyr@gmail.com>
2023-08-09 19:47:39 -0500

    first commit

---
+++ src/main/java/com/example/demo/PwAuthU4JyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAuthU4JyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAuthU4JyApplication.class, args);
+	}
+
+}
